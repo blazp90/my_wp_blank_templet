b042f7d1cdaf485cf9352302154f04c255089b5e
blazp90 <blazpoglovnik@hotmail.com>
2016-12-17 09:44:34 +0100

    seting templet files

---
+++ function.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once get_templet_directory() . "includs/inc.php";
+
+
+/***** EDIT THIS FILE ON YOUR OWN RISK *******/
---
+++ includs/inc.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once get_template_directory() . '/inc/init.php';
+
+
+/***** EDIT THIS FILE ON YOUR OWN RISK *******/
+?>
