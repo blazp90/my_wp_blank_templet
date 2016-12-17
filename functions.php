c726409168a1909e4520ceb5cbdd88005415de0b
blazp90 <blazpoglovnik@hotmail.com>
2016-12-17 14:27:34 +0100

    some changes

--- functions.php
+++ functions.php
@@ -18,3 +18,67 @@ add_action('wp_print_styles', 'add_google_fonts');
 
 // WordPress Titles
 add_theme_support( 'title-tag' );
+
+// Support Featured Images
+add_theme_support( 'post-thumbnails' );
+
+// Custom Post Type
+function create_my_custom_post() {
+	register_post_type('my-custom-post',
+			array(
+			'labels' => array(
+					'name' => __('My Custom Post'),
+					'singular_name' => __('My Custom Post'),
+			),
+			'public' => true,
+			'has_archive' => true,
+			'supports' => array(
+					'title',
+					'editor',
+					'thumbnail',
+				  'custom-fields'
+			)
+	));
+}
+add_action('init', 'create_my_custom_post');
+
+
+
+/********Settings page***********/
+// Custom settings
+function custom_settings_add_menu() {
+  add_menu_page( 'Custom Settings', 'Custom Settings', 'manage_options', 'custom-settings', 'custom_settings_page', null, 99);
+}
+add_action( 'admin_menu', 'custom_settings_add_menu' );
+// Create Custom Global Settings
+function custom_settings_page() { ?>
+  <div class="wrap">
+    <h1>Custom Settings</h1>
+    <form method="post" action="options.php">
+       <?php
+           settings_fields('section');
+           do_settings_sections('theme-options');      
+           submit_button(); 
+       ?>          
+    </form>
+  </div>
+<?php }
+
+// Twitter
+function setting_twitter() { ?>
+  <input type="text" name="twitter" id="twitter" value="<?php echo get_option('twitter'); ?>" />
+<?php }
+
+function setting_github() { ?>
+  <input type="text" name="github" id="github" value="<?php echo get_option('github'); ?>" />
+<?php }
+
+function custom_settings_page_setup() {
+  add_settings_section('section', 'All Settings', null, 'theme-options');
+  add_settings_field('twitter', 'Twitter URL', 'setting_twitter', 'theme-options', 'section');
+  add_settings_field('github', 'GitHub URL', 'setting_github', 'theme-options', 'section');
+
+  register_setting('section', 'twitter');
+  register_setting('section', 'github');
+}
+add_action( 'admin_init', 'custom_settings_page_setup' );
